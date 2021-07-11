7e4059d6b7ceccb5f9c64d93b141bc9dceea0cf6
Bruno Militzer <brunomilitzer.work@gmail.com>
2021-07-11 21:37:31 +0100

    Added Spring Security

--- eventmanagement-api/src/main/java/com/brunomilitzer/eventmanagement/repositories/EventRepository.java
+++ eventmanagement-api/src/main/java/com/brunomilitzer/eventmanagement/repositories/EventRepository.java
@@ -7,6 +7,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.security.access.prepost.PreAuthorize;
 
 import java.time.ZoneId;
 
@@ -16,5 +17,9 @@ public interface EventRepository extends PagingAndSortingRepository<Event, Long>
     Page<Event> findByName( @Param( "name" ) String name, Pageable pageable );
 
     Page<Event> findByNameAndZoneId( @Param( "name" ) String name, @Param( "zoneId" ) ZoneId zoneId, Pageable pageable );
+
+    @PreAuthorize( "hasRole('ROLE_ADMIN')" )
+    @Override
+    void deleteById( Long id );
 
 }
